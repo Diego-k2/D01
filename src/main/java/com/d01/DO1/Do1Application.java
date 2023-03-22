4c9028f86b23754e63749571e79f4a8b1eabe286
Diego-k2 <dSilva@netsoft.com>
2023-03-22 08:50:38 -0300

    Commit inicial

---
+++ src/main/java/com/d01/DO1/Do1Application.java
@@ -0,0 +1,13 @@
+package com.d01.DO1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Do1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Do1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/d01/DO1/Do1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d01.DO1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Do1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
